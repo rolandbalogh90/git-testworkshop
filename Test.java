f845fb47ff921eea4b6cbd5dbb844b858c5164f0
rolandbalogh <roland.balogh@liferay.com>
2019-09-19 12:15:14 +0200

    LPS-12345 Changed Public to Private

--- Test.java
+++ Test.java
@@ -1,4 +1,4 @@
-public static double getRandomDoubleBetweenRange(double min, double max){
+private static double getRandomDoubleBetweenRange(double min, double max){
     double x = (Math.random()*((max-min)+1))+min;
     return x;
 }
