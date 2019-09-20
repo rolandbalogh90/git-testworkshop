716c70302cf2025b3ad6a9edd59062fb05f9a92b
Tibor Lipusz <tibor.lipusz@liferay.com>
2019-09-20 11:32:13 +0200

    LPS-88888 My fix

--- Test.java
+++ Test.java
@@ -1,5 +1,8 @@
 private static double getRandomDoubleBetweenRange(double min, double max){
     double x = (Math.random()*((max-min)+1))+min;
     return x;
+
+    System.out.println("hello");
+
    console.log(x);
 }
