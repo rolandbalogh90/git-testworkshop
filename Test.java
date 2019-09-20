9d8753a2df9470b4635b1b1fca11270a9b53c64b
Tibor Lipusz <tibor.lipusz@liferay.com>
2019-09-20 12:09:04 +0200

    LPS-88888 More fix

--- Test.java
+++ Test.java
@@ -1,7 +1,8 @@
 private static double getRandomDoubleBetweenRange(double min, double max){
-    double x = (Math.random()*((max-min)+1))+min;
     return x;
+
     System.out.println("hello");
+    double x = (Math.random()*((max-min)+1))+min;
 
    console.log(x);
 }
